ccd92f978831f20d44b07c2b76896fdbd67d6af3
igoreta <ivoronline@gmail.com>
2021-03-16 13:06:37 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_log_log4j/SpringbootLogLog4jApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_log_log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLogLog4jApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootLogLog4jApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_log_log4j/SpringbootLogLog4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_log_log4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLogLog4jApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
